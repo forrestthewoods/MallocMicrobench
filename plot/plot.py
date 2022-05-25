31d656c9613683a3e2ff88669ace02d3a2b0a5a3
Forrest Smith <forrestthewoods@gmail.com>
2022-05-24 21:41:35 -0700

    Pretty colors in data viz chart

--- plot/plot.py
+++ plot/plot.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from matplotlib.ticker import LogFormatter
 import math
 import csv
+
+maxEntries = 0
 
 kilobyte = 1024.0
 megabyte = kilobyte * 1024.0
@@ -19,18 +23,25 @@ def format_nanoseconds(ns):
 def format_bytes(bytes):
     if bytes < 1024:
         return f"{bytes:.0f} bytes"
+    elif bytes == 1024:
+        return "1 kilobyte"
     elif bytes < 1024 * 1024:
         return f"{bytes/1024:.0f} kilobytes"
+    elif bytes == 1024 * 1024:
+        return "1 megabyte" 
     elif bytes < 1024 * 1024 * 1024:
-        return f"{bytes/1024/1024:.2f} megabytes"
+        return f"{bytes/1024/1024} megabytes"
     else:
-        return f"{float(bytes)/1024.0/1024.0/1024.0:.2f} gigabytes"
+        return f"{1024.0/1024.0/1024.0} gigabytes"
+
+class ColorbarFormatter(LogFormatter):
+    def __call__(self, x, pos = None):
+        return format_bytes(x)
 
 def main():
     # Parse data
-    maxEntries = 100000
     chart_title = "Doom 3 Memory Analysis - Malloc"
-    mallocMax = 50 * megabyte
+    mallocMax = 100 * megabyte
     mallocMaxLog = math.log(mallocMax)
 
     print("Parsing data")
@@ -67,7 +78,11 @@ def main():
 
     # Normalize allocSizes to [0,1]
     # Used fix upper limited that clamped
-    normalizedAllocSizes = [clamp(math.log(min(alloc, mallocMax))/mallocMaxLog,0.0,1.0) for alloc in allocSizes]
+    def normalize_z(v):
+        clamp(math.log(min(v, mallocMax))/mallocMaxLog, 0.0, 1.0)
+
+    normalizedAllocSizes = [normalize_z(alloc) for alloc in allocSizes]
+    clampedAllocSizes = [min(alloc, mallocMax) for alloc in allocSizes]
 
     # Scatter plot
     if True:
@@ -79,43 +94,30 @@ def main():
         def y_labels(tick, pos):
             return format_nanoseconds(tick)
 
+        def z_labels(tick, pos):
+            return format_bytes(tick)
+
+        cbar_ticks = [32,64,128,256,512,kilobyte,kilobyte*10, kilobyte*100, megabyte, megabyte*10, mallocMax]
+
 
         fig,ax = plt.subplots(1,1, figsize=(16,9))
-        plt.scatter(x=timestamps, y=allocTimes, c=normalizedAllocSizes, s=0.1, cmap='jet')
+        plt.scatter(
+            x=timestamps, 
+            y=allocTimes,
+            c=clampedAllocSizes, 
+            s=0.2, 
+            cmap='nipy_spectral', 
+            norm=colors.LogNorm(vmin=None,vmax=mallocMax))
         plt.semilogy(basey=10)
         ax.xaxis.set_major_formatter(FuncFormatter(x_labels))
         ax.yaxis.set_major_formatter(FuncFormatter(y_labels))
         ax.set_ylabel("Malloc Time")
         ax.set_xlabel("Game time (seconds)")
         ax.set_title(chart_title)
-
-        # Define a color bar to provide a third dimension (alloc size)
-        num_cbar_ticks = int(10)
-
-        print(f"Debug 0: {format_bytes(mallocMax)}")
-        
-        debug = [mallocMax for i in range(num_cbar_ticks)]
-        print(f"Debug 1: {debug}")
-
-        debug = [i for i in range(num_cbar_ticks)]
-        print(f"Debug 2: {debug}")
-
-        debug = [0 if i == 0 else i/num_cbar_ticks for i in range(num_cbar_ticks + 1)]
-        print(f"Debug 3: {debug}")
-
-        debug = [0 if i == 0 else i/num_cbar_ticks*mallocMax for i in range(num_cbar_ticks + 1)]
-        print(f"Debug 5: {debug}")
-
-        debug = [0 if i == 0 else math.log(i/num_cbar_ticks*mallocMax) for i in range(num_cbar_ticks + 1)]
-        print(f"Debug 6: {debug}")
-        
-        debug = [format_bytes(b) for b in debug]
-        print(f"Debug 7: {debug}")
-
-        cbar_labels = ["0" if i == 0 else format_bytes(math.log(i*mallocMax)) for i in range(num_cbar_ticks)]
-        print(f"Cbar Labels: {cbar_labels}")
-        #cbar_labels = [str(i) for i in range(num_cbar_ticks)]
-        cbar = plt.colorbar()
+        #ax.set_facecolor('#FFF1E6') # financial times
+        ax.set_facecolor('#101010')
+        #cbar = plt.colorbar()
+        cb = plt.colorbar(ticks=cbar_ticks, format=ColorbarFormatter())
         #cbar = plt.colorbar(ticks=[i for i in range(num_cbar_ticks)])
         #cbar.ax.set_yticklabels(cbar_labels)
 
